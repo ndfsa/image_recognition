7ffc8b6fbde165fd4cc6f1c9bf637de8d6c72558
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-11-08 14:25:36 -0500

    Added output

--- launch.py
+++ launch.py
@@ -9,7 +9,8 @@ def recognize(arg):
 	detector.loadModel()
 	res_path = os.path.splitext(arg)[0] + "_res" + os.path.splitext(arg)[1]
 	detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , './uploads/' + arg), output_image_path=os.path.join(execution_path , './uploads/' + res_path))
-
+	f = open("./output/" + res_path + ".txt", "a")
 	for eachObject in detections:
+		print(eachObject["name"] , " : " , eachObject["percentage_probability"], file=f)
 		print(eachObject["name"] , " : " , eachObject["percentage_probability"])
 	return res_path
