3e3d808babcedf82d5d55c810911978d0bcad4b5
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-11-08 13:38:22 -0500

    Firts commit

---
+++ launch.py
@@ -0,0 +1,15 @@
+from imageai.Detection import ObjectDetection
+import os
+
+def recognize(arg):
+	execution_path = os.getcwd()
+	detector = ObjectDetection()
+	detector.setModelTypeAsRetinaNet()
+	detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
+	detector.loadModel()
+	res_path = os.path.splitext(arg)[0] + "_res" + os.path.splitext(arg)[1]
+	detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , './uploads/' + arg), output_image_path=os.path.join(execution_path , './uploads/' + res_path))
+
+	for eachObject in detections:
+		print(eachObject["name"] , " : " , eachObject["percentage_probability"])
+	return res_path
